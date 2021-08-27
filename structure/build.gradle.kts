a357b9feed639e733ecd908944ea9a9ccc2f6fa8
Penn (Dapeng) Zhang <dapeng.zhang@gmail.com>
2021-08-26 22:28:11 -0700

    reorder dependency

--- structure/build.gradle.kts
+++ structure/build.gradle.kts
@@ -20,8 +20,8 @@ dependencies {
   implementation("com.google.code.findbugs:jsr305:3.0.2")
   testImplementation("com.google.truth:truth:1.0.1")
   testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
-  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
   testImplementation("org.mockito:mockito-core:3.3.3")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
 }
 
 tasks {
