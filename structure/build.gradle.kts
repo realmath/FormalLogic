f2570ff1a33954c72764ae4b32d865bb7661bcb7
dapengzhang0 <dapeng.zhang@gmail.com>
2022-08-26 22:25:42 -0700

    use spotless and bump dep versions

--- structure/build.gradle.kts
+++ structure/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   java
-  id("com.github.sherter.google-java-format") version "0.9"
-  id("net.ltgt.errorprone") version "1.1.1"
+  id("com.diffplug.spotless") version "6.10.0"
+  id("net.ltgt.errorprone") version "2.0.2"
 }
 
 repositories {
@@ -14,17 +14,23 @@ java {
 }
 
 dependencies {
-  annotationProcessor("org.projectlombok:lombok:1.18.12")
-  compileOnly("org.projectlombok:lombok:1.18.12")
-  errorprone("com.google.errorprone:error_prone_core:2.3.4")
+  annotationProcessor("org.projectlombok:lombok:1.18.24")
+  compileOnly("org.projectlombok:lombok:1.18.24")
+  errorprone("com.google.errorprone:error_prone_core:2.15.0")
   implementation("com.google.code.findbugs:jsr305:3.0.2")
-  testAnnotationProcessor("org.projectlombok:lombok:1.18.12")
-  testImplementation("com.google.guava:guava-testlib:31.0-jre")
-  testImplementation("com.google.truth:truth:1.0.1")
-  testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
-  testImplementation("org.mockito:mockito-core:3.3.3")
-  testCompileOnly("org.projectlombok:lombok:1.18.12")
-  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
+  testAnnotationProcessor("org.projectlombok:lombok:1.18.24")
+  testImplementation("com.google.guava:guava-testlib:31.1-jre")
+  testImplementation("com.google.truth:truth:1.1.3")
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+  testImplementation("org.mockito:mockito-core:4.7.0")
+  testCompileOnly("org.projectlombok:lombok:1.18.24")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+}
+
+spotless {
+  java {
+    googleJavaFormat()
+  }
 }
 
 tasks {
