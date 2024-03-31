a090a95db7c2891320d0d14667b5be0e7ae06615
Dapeng Zhang <dapeng.zhang@gmail.com>
2024-03-31 14:27:36 -0700

    AbstractBinaryOperatorTest

---
+++ structure/src/test/java/formallogic/structure/common/AbstractBinaryOperatorTest.java
@@ -0,0 +1,83 @@
+package formallogic.structure.common;
+
+import static com.google.common.truth.Truth.assertThat;
+import static formallogic.structure.domains.TruthDomain.TRUTH_DOMAIN;
+
+import com.google.common.testing.EqualsTester;
+import formallogic.structure.core.Domain;
+import formallogic.structure.core.Term;
+import formallogic.structure.core.Variable;
+import formallogic.structure.testing.FakeDomain;
+import formallogic.structure.testing.ProdTerm;
+import formallogic.structure.testing.UnaryTerm;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+
+public abstract class AbstractBinaryOperatorTest {
+
+  protected abstract List<Class<? extends AbstractBinaryOperator<?>>> classes();
+
+  protected abstract AbstractBinaryOperator<?> newInstance(
+      Class<? extends AbstractBinaryOperator<?>> cls, Term left, Term right) throws Exception;
+
+  @Test
+  public void beanTest() throws Exception {
+    Domain vLeftDomain = new FakeDomain();
+    Variable vLeft = new Variable(vLeftDomain);
+    Domain vRight1Domain = new FakeDomain();
+    Variable vRight1 = new Variable(vRight1Domain);
+    Domain vRight2Domain = new FakeDomain();
+    Variable vRight2 = new Variable(vRight2Domain);
+    Term left = new UnaryTerm(vLeft, TRUTH_DOMAIN);
+    Term right = new ProdTerm(vRight1, vRight2, TRUTH_DOMAIN);
+    for (Class<? extends AbstractBinaryOperator<?>> cls : classes()) {
+      AbstractBinaryOperator<?> b = newInstance(cls, left, right);
+      assertThat(b.leftOperand()).isEqualTo(left);
+      assertThat(b.rightOperand()).isEqualTo(right);
+      assertThat(b.variables()).isEqualTo(Set.of(vLeft, vRight1, vRight2));
+    }
+  }
+
+  @Test
+  public void substitute() throws Exception {
+    Domain vLeftDomain = new FakeDomain();
+    Variable vLeft = new Variable(vLeftDomain);
+    Domain vRight1Domain = new FakeDomain();
+    Variable vRight1 = new Variable(vRight1Domain);
+    Domain vRight2Domain = new FakeDomain();
+    Variable vRight2 = new Variable(vRight2Domain);
+    Term left = new UnaryTerm(vLeft, TRUTH_DOMAIN);
+    Term right = new ProdTerm(vRight1, vRight2, TRUTH_DOMAIN);
+    Term replacement = new UnaryTerm(left, vRight1Domain);
+    for (Class<? extends AbstractBinaryOperator<?>> cls : classes()) {
+      AbstractBinaryOperator<?> b = newInstance(cls, left, right);
+      assertThat(b.substitute(vRight1, replacement))
+          .isEqualTo(newInstance(cls, left, right.substitute(vRight1, replacement)));
+    }
+  }
+
+  @Test
+  public void equalsTest() throws Exception {
+    Domain vLeftDomain = new FakeDomain();
+    Variable vLeft = new Variable(vLeftDomain);
+    Domain vRightDomainA = new FakeDomain();
+    Variable vRightA = new Variable(vRightDomainA);
+    Domain vRightDomainB = new FakeDomain();
+    Variable vRightB = new Variable(vRightDomainB);
+    Term left1 = new UnaryTerm(vLeft, TRUTH_DOMAIN);
+    Term right1 = new ProdTerm(vRightA, vRightB, TRUTH_DOMAIN);
+    Term left2 = new UnaryTerm(vLeft, TRUTH_DOMAIN);
+    Term right2 = new ProdTerm(vRightA, vRightB, TRUTH_DOMAIN);
+    Term right3 = new ProdTerm(vRightB, vRightA, TRUTH_DOMAIN);
+    EqualsTester equalsTester = new EqualsTester();
+    for (Class<? extends AbstractBinaryOperator<?>> cls : classes()) {
+      AbstractBinaryOperator<?> b1 = newInstance(cls, left1, right1);
+      AbstractBinaryOperator<?> b2 = newInstance(cls, left2, right2);
+      AbstractBinaryOperator<?> b3 = newInstance(cls, left1, right3);
+      equalsTester.addEqualityGroup(b1, b2);
+      equalsTester.addEqualityGroup(b3);
+    }
+    equalsTester.testEquals();
+  }
+}
--- structure/src/test/java/formallogic/structure/formula/BinaryConnectiveTest.java
+++ structure/src/test/java/formallogic/structure/formula/BinaryConnectiveTest.java
@@ -1,89 +1,23 @@
 package formallogic.structure.formula;
 
-import static com.google.common.truth.Truth.assertThat;
-import static formallogic.structure.domains.TruthDomain.TRUTH_DOMAIN;
-
-import com.google.common.testing.EqualsTester;
 import formallogic.structure.common.AbstractBinaryOperator;
-import formallogic.structure.core.Domain;
+import formallogic.structure.common.AbstractBinaryOperatorTest;
 import formallogic.structure.core.Term;
-import formallogic.structure.core.Variable;
-import formallogic.structure.testing.FakeDomain;
-import formallogic.structure.testing.ProdTerm;
-import formallogic.structure.testing.UnaryTerm;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
-import org.junit.jupiter.api.Test;
 
-final class BinaryConnectiveTest {
-  private final List<Class<? extends BinaryConnective>> classes =
+final class BinaryConnectiveTest extends AbstractBinaryOperatorTest {
+  private final List<Class<? extends AbstractBinaryOperator<?>>> classes =
       Arrays.asList(Conjunction.class, Disjunction.class, Equality.class, Implication.class);
 
-  @Test
-  void beanTest() throws Exception {
-    Domain vLeftDomain = new FakeDomain();
-    Variable vLeft = new Variable(vLeftDomain);
-    Domain vRight1Domain = new FakeDomain();
-    Variable vRight1 = new Variable(vRight1Domain);
-    Domain vRight2Domain = new FakeDomain();
-    Variable vRight2 = new Variable(vRight2Domain);
-    Term left = new UnaryTerm(vLeft, TRUTH_DOMAIN);
-    Term right = new ProdTerm(vRight1, vRight2, TRUTH_DOMAIN);
-    for (Class<? extends AbstractBinaryOperator<?>> cls : classes) {
-      AbstractBinaryOperator<?> b =
-          cls.getDeclaredConstructor(Term.class, Term.class).newInstance(left, right);
-      assertThat(b.leftOperand()).isEqualTo(left);
-      assertThat(b.rightOperand()).isEqualTo(right);
-      assertThat(b.variables()).isEqualTo(Set.of(vLeft, vRight1, vRight2));
-    }
+  @Override
+  protected List<Class<? extends AbstractBinaryOperator<?>>> classes() {
+    return classes;
   }
 
-  @Test
-  void substitute() throws Exception {
-    Domain vLeftDomain = new FakeDomain();
-    Variable vLeft = new Variable(vLeftDomain);
-    Domain vRight1Domain = new FakeDomain();
-    Variable vRight1 = new Variable(vRight1Domain);
-    Domain vRight2Domain = new FakeDomain();
-    Variable vRight2 = new Variable(vRight2Domain);
-    Term left = new UnaryTerm(vLeft, TRUTH_DOMAIN);
-    Term right = new ProdTerm(vRight1, vRight2, TRUTH_DOMAIN);
-    Term replacement = new UnaryTerm(left, vRight1Domain);
-    for (Class<? extends AbstractBinaryOperator<?>> cls : classes) {
-      AbstractBinaryOperator<?> b =
-          cls.getDeclaredConstructor(Term.class, Term.class).newInstance(left, right);
-      assertThat(b.substitute(vRight1, replacement))
-          .isEqualTo(
-              cls.getDeclaredConstructor(Term.class, Term.class)
-                  .newInstance(left, right.substitute(vRight1, replacement)));
-    }
-  }
-
-  @Test
-  void equalsTest() throws Exception {
-    Domain vLeftDomain = new FakeDomain();
-    Variable vLeft = new Variable(vLeftDomain);
-    Domain vRightDomainA = new FakeDomain();
-    Variable vRightA = new Variable(vRightDomainA);
-    Domain vRightDomainB = new FakeDomain();
-    Variable vRightB = new Variable(vRightDomainB);
-    Term left1 = new UnaryTerm(vLeft, TRUTH_DOMAIN);
-    Term right1 = new ProdTerm(vRightA, vRightB, TRUTH_DOMAIN);
-    Term left2 = new UnaryTerm(vLeft, TRUTH_DOMAIN);
-    Term right2 = new ProdTerm(vRightA, vRightB, TRUTH_DOMAIN);
-    Term right3 = new ProdTerm(vRightB, vRightA, TRUTH_DOMAIN);
-    EqualsTester equalsTester = new EqualsTester();
-    for (Class<? extends AbstractBinaryOperator<?>> cls : classes) {
-      AbstractBinaryOperator<?> b1 =
-          cls.getDeclaredConstructor(Term.class, Term.class).newInstance(left1, right1);
-      AbstractBinaryOperator<?> b2 =
-          cls.getDeclaredConstructor(Term.class, Term.class).newInstance(left2, right2);
-      AbstractBinaryOperator<?> b3 =
-          cls.getDeclaredConstructor(Term.class, Term.class).newInstance(left1, right3);
-      equalsTester.addEqualityGroup(b1, b2);
-      equalsTester.addEqualityGroup(b3);
-    }
-    equalsTester.testEquals();
+  @Override
+  protected AbstractBinaryOperator<?> newInstance(
+      Class<? extends AbstractBinaryOperator<?>> cls, Term left, Term right) throws Exception {
+    return cls.getDeclaredConstructor(Term.class, Term.class).newInstance(left, right);
   }
 }
