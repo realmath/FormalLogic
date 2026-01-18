9b1cd33ef7ad9476e55c8a07df97fdc149316c8a
Dapeng Zhang <dapeng.zhang@gmail.com>
2026-01-17 21:57:38 -0800

    upgrade to Java 17

--- structure/build.gradle.kts
+++ structure/build.gradle.kts
@@ -8,19 +8,19 @@ repositories {
 }
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_11
-  targetCompatibility = JavaVersion.VERSION_11
+  sourceCompatibility = JavaVersion.VERSION_17
+  targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
-  annotationProcessor("org.projectlombok:lombok:1.18.24")
-  compileOnly("org.projectlombok:lombok:1.18.24")
+  annotationProcessor("org.projectlombok:lombok:1.18.42")
+  compileOnly("org.projectlombok:lombok:1.18.42")
   testAnnotationProcessor("org.projectlombok:lombok:1.18.24")
-  testImplementation("com.google.guava:guava-testlib:32.0.0-jre")
+  testImplementation("com.google.guava:guava-testlib:33.5.0-jre")
   testImplementation("com.google.truth:truth:1.1.4")
   testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
   testImplementation("org.mockito:mockito-core:4.7.0")
-  testCompileOnly("org.projectlombok:lombok:1.18.24")
+  testCompileOnly("org.projectlombok:lombok:1.18.42")
   testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 }
 
